0cd6d501f41e1613eb598aeffc901c96b2d0acf5
Ryan Crowley <ryancrowleycode@gmail.com>
2019-11-01 14:46:26 -0500

    boiler-plate html and added code from class to main.js

---
+++ src/scripts/main.js
@@ -0,0 +1,1 @@
+// Lightning Exercise 1: Create an object that represents a bill from your doctor's office. It should have the following properties: officeName, streetAddress, doctorName, patientName, visitDate, amountBilled, dueDate. You provide example values for each one.
