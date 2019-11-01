ed94efd576688a0607d6ffcde6f7039a22c6e0c2
Ryan Crowley <ryancrowleycode@gmail.com>
2019-11-01 14:53:30 -0500

    lightning two done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,12 +8,23 @@
 
 // You provide example values for each one.
 
+const dateVisited = "visitDate"
+const owed = "amountBilled"
+const patient = "patientName"
+
 const bill = {
     officeName: "Doctors R Us",
     streetAddress: "123 Sesame Street",
     doctorName: "Nick",
     patientName: "Tickle Me Elmo",
-    visitDate: "2019-11-01"
+    visitDate: "2019-11-01",
     amountBilled: 247,
     dueDate: "2019-12-01"
 }
+
+// Lightning Exercise 2: Copy the code below and paste it above your object.
+// Use square bracket notation to output the value of those three properties to the console in Chrome.
+
+console.log("Date Visited: ", bill[dateVisited])
+console.log("Amount owed: ", bill[owed])
+console.log("Patient: ", bill[patient])
