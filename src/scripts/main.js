234a0cecf7e729fa96f32c08c8fe6a745adb34a8
Ryan Crowley <ryancrowleycode@gmail.com>
2019-11-01 14:49:54 -0500

    lightning one complete, object for bill has been created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
-// Lightning Exercise 1: Create an object that represents a bill from your doctor's office. It should have the following properties: officeName, streetAddress, doctorName, patientName, visitDate, amountBilled, dueDate. You provide example values for each one.
+// Lightning Exercise 1: Create an object that represents a bill from your doctor's office. It should have the following properties: officeName: 
+// streetAddress: 
+// doctorName: 
+// patientName: 
+// visitDate: 
+// amountBilled: 
+// dueDate:
+
+// You provide example values for each one.
+
+const bill = {
+    officeName: "Doctors R Us",
+    streetAddress: "123 Sesame Street",
+    doctorName: "Nick",
+    patientName: "Tickle Me Elmo",
+    visitDate: "2019-11-01"
+    amountBilled: 247,
+    dueDate: "2019-12-01"
+}
