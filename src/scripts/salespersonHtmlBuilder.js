371395b4c951273e72de1ad4c88f0a3adcf6ef32
Ryan Crowley <ryancrowleycode@gmail.com>
2019-11-06 09:10:29 -0600

    using modules. Data in data.js. Function for creating HTML template for a salesperson is in salespersonHtmlBuilder.js.

---
+++ src/scripts/salespersonHtmlBuilder.js
@@ -0,0 +1,17 @@
+// Here's his requirements to display for each sale.
+
+// Display the first and last name of the sales agent.
+// Display all keys and values for the car sold.
+// Display the gross profit made on the sale.
+
+const createSalespersonHtml = () => {
+    // Name
+    // Year
+    // Model
+    // Make
+    // Color
+    // Profit
+    // hr
+}
+
+export default createSalespersonHtml
